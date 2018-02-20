b593851149ea2e0d7bfdc39587e81860be1b61a7
Tim Johnson <tjohnson@scitent.com>
2018-02-20 13:36:44 -0500

    added a rise package

---
+++ rise/tc-config.js
@@ -0,0 +1,22 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://DCv8XP4F0h7AW0B7bpSQ-SfZYkar5xut_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+    "en-US": "5- Ultrasound Artifacts: Image Optimization"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+"en-US": "&lt;p dir=&#34;ltr&#34;&gt;&lt;strong&gt;&lt;em&gt;NOTE: This material is best understood after completing The Physics of Ultrasound series first.&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p dir=&#34;ltr&#34;&gt;Ultrasound artifacts, which are becoming increasingly rare as technology improves, can still lead to critical errors in interpretation if they are not recognized and dealt with appropriately. A proper understanding of how they are created allows the echocardiographer to recognize, and work around these potentially hazardous pitfalls.&lt;/p&gt;&lt;p dir=&#34;ltr&#34;&gt;&lt;br&gt;&lt;/p&gt;&lt;p dir=&#34;ltr&#34;&gt;&lt;strong&gt;In this course, you will learn about recommendations for image optimization, including:&lt;/strong&gt;&lt;/p&gt;&lt;ul&gt;&lt;li dir=&#34;ltr&#34;&gt;Gastric decompression&amp;nbsp;&lt;/li&gt;&lt;li dir=&#34;ltr&#34;&gt;Use of harmonic imaging and system gain&lt;/li&gt;&lt;li dir=&#34;ltr&#34;&gt;Finding the optimum depth, width, zoom, and focus&lt;/li&gt;&lt;li dir=&#34;ltr&#34;&gt;Dynamic range&lt;/li&gt;&lt;li dir=&#34;ltr&#34;&gt;Color gain&lt;/li&gt;&lt;li dir=&#34;ltr&#34;&gt;Variance mapping&lt;/li&gt;&lt;/ul&gt;&lt;p dir=&#34;ltr&#34;&gt;In addition to understanding what is normal and not normal, as well as the technical basis for artifacts, important strategies for minimizing ultrasound artifacts include the use of multiple complementary ultrasound windows and by making appropriate changes to machine settings.&lt;/p&gt;&lt;p dir=&#34;ltr&#34;&gt;&lt;br&gt;&lt;/p&gt;&lt;p dir=&#34;ltr&#34;&gt;&lt;strong&gt;&lt;span style=&#34;color: rgb(0, 0, 0);&#34;&gt;Important: Estimated time to complete this course is 30 minutes.&amp;nbsp;&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Credit awarded: .5 credits/contact hours&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Disclosure: Dr. Thiele disclosed no financial relationship or interest with any proprietary entity producing healthcare goods or services.&lt;/strong&gt;&lt;/p&gt;"
+};
+
+TC_RECORD_STORES = [];
